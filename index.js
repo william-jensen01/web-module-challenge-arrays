9110853a02c8f6b6c52f70a8b3aae101b02ae06b
William Jensen <williambjensen01@gmail.com>
2020-07-27 21:40:41 -0600

    Task 4

--- index.js
+++ index.js
@@ -92,11 +92,10 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
-
-    /*code here*/
-
+function getFlavorByIndex(array,index){
+    return array[index];
 }
+console.log(getFlavorByIndex(originalFlavors,2));
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
